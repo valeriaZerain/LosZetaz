beb553fafcf0215359293e704c84cf9010fe6579
root <zein.tonconi@gmail.com>
2023-10-19 09:02:04 -0400

    Prueba commit

--- app/src/main/java/com/progra/loszetaz/MainActivity.kt
+++ app/src/main/java/com/progra/loszetaz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.progra.loszetaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+// Prueba commit Zein
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
