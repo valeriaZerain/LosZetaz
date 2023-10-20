45d59be46982bdafbc8cdfdeae4d1af1dc98dc95
root <zein.tonconi@gmail.com>
2023-10-19 21:15:27 -0400

    Club Data Class created

--- app/src/main/java/com/progra/loszetaz/MainActivity.kt
+++ app/src/main/java/com/progra/loszetaz/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("xD Marian Carajoo")
         println(UserDB.getAllUsers())
         println(UserDB.getUserById(10))
         println(UserDB.getUserById(0) ?: "No existe el usuario")
