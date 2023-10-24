07e9effe231ef5ba7541dffb090cc8fcc93443ab
root <zein.tonconi@gmail.com>
2023-10-24 09:44:31 -0400

    Drop down menu

--- app/src/main/java/com/progra/loszetaz/PantallaBusquedaActivity.kt
+++ app/src/main/java/com/progra/loszetaz/PantallaBusquedaActivity.kt
@@ -2,10 +2,27 @@ package com.progra.loszetaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class PantallaBusquedaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_busqueda)
+
+        val zones = resources.getStringArray(R.array.zones)
+
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.dropdown_zones)
+
+        val adapter = ArrayAdapter(this, R.layout.dropdown_item, zones)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener {
+            adapterView, view, i, l ->
+                val itemSelected = adapterView.get
+        }
     }
 }
