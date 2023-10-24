271156d2aea5279e2ccb3b18e4681c2f31115039
root <zein.tonconi@gmail.com>
2023-10-24 08:52:32 -0400

    Checkboxes Filters

--- app/src/main/java/com/progra/loszetaz/PantallaBusquedaActivity.kt
+++ app/src/main/java/com/progra/loszetaz/PantallaBusquedaActivity.kt
@@ -2,15 +2,10 @@ package com.progra.loszetaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.progra.loszetaz.dataBase.ClubDB
-import com.progra.loszetaz.dataBase.UserDB
 
-
-class MainActivity : AppCompatActivity() {
+class PantallaBusquedaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_pantalla_busqueda)
     }
 }
