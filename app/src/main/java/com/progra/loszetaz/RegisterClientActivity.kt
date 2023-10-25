d76790ac0d765c1718234fc0715215be8d74bfaf
vale_ <valeria.zerain@gmail.com>
2023-10-25 09:33:17 -0400

    Crate account in firebase

--- app/src/main/java/com/progra/loszetaz/RegisterClientActivity.kt
+++ app/src/main/java/com/progra/loszetaz/RegisterClientActivity.kt
@@ -2,8 +2,11 @@ package com.progra.loszetaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.progra.loszetaz.databinding.ActivityRegisterClientBinding
 
-class RegisterClient : AppCompatActivity() {
+class RegisterClientActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_client)
