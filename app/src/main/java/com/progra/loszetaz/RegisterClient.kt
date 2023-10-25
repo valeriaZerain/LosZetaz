0fd0cc7035d30474938d66a452c84dbbd65154de
vale_ <valeria.zerain@gmail.com>
2023-10-24 20:13:35 -0400

    Register in XML

---
+++ app/src/main/java/com/progra/loszetaz/RegisterClient.kt
@@ -0,0 +1,11 @@
+package com.progra.loszetaz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterClient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_client)
+    }
+}
