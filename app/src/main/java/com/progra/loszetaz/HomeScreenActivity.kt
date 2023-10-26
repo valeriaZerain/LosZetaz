1618fe3f8dcb9c8463abf6b4177fc200bc21093a
root <zein.tonconi@gmail.com>
2023-10-25 20:41:55 -0400

    Show Most Liked Clubs in Home Screen

--- app/src/main/java/com/progra/loszetaz/HomeScreenActivity.kt
+++ app/src/main/java/com/progra/loszetaz/HomeScreenActivity.kt
@@ -2,10 +2,30 @@ package com.progra.loszetaz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.progra.loszetaz.adapters.MostLikedClubAdapter
+import com.progra.loszetaz.dataBase.ClubDB
+import com.progra.loszetaz.databinding.ActivityHomeScreenBinding
 
 class HomeScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeScreenBinding
+    private val mostLikedClubAdapter by lazy { MostLikedClubAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showMostLikedClubs()
+    }
+
+    fun showMostLikedClubs(){
+
+        mostLikedClubAdapter.addClubs(ClubDB.mostLikedClubs())
+        binding.mostLikedClubRecylcer.apply{
+            layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = mostLikedClubAdapter
+        }
+
     }
 }
