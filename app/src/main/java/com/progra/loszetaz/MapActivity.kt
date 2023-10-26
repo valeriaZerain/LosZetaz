236ca9a1a404e27b2d08bc4fccf69a576b405e28
vale_ <valeria.zerain@gmail.com>
2023-10-26 17:27:57 -0400

    Creación del fragment para el uso de mapas

--- app/src/main/java/com/progra/loszetaz/MapActivity.kt
+++ app/src/main/java/com/progra/loszetaz/MapActivity.kt
@@ -1,5 +1,7 @@
 package com.progra.loszetaz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
@@ -8,6 +10,7 @@ import com.progra.loszetaz.fragment.MapsFragment
 
 class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
+    val context:Context=this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
@@ -18,6 +21,10 @@ class MapActivity : AppCompatActivity() {
             setReorderingAllowed(true)
             addToBackStack("replacement")
         }
-    }
 
+        binding.imageSearch.setOnClickListener{
+            val intent=Intent(this, SearchScreenActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/progra/loszetaz/fragment/MapsFragment.kt
+++ app/src/main/java/com/progra/loszetaz/fragment/MapsFragment.kt
@@ -1,5 +1,7 @@
 package com.progra.loszetaz.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -18,15 +20,6 @@ import com.progra.loszetaz.R
 class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
