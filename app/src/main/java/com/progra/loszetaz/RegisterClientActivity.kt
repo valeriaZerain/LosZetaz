229d187631f361ba41014fd93d5a8620460d5dfd
vale_ <valeria.zerain@gmail.com>
2023-10-25 21:07:37 -0400

    Interaccion de pantallas para crear usuarios

--- app/src/main/java/com/progra/loszetaz/RegisterClientActivity.kt
+++ app/src/main/java/com/progra/loszetaz/RegisterClientActivity.kt
@@ -1,14 +1,44 @@
 package com.progra.loszetaz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.progra.loszetaz.databinding.ActivityRegisterClientBinding
 
 class RegisterClientActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegisterClientBinding
+    val context: Context = this
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_client)
+        val binding = ActivityRegisterClientBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        binding.buttonCreateaccount.setOnClickListener {
+            var email:String= binding.edittextEmail.text.toString()
+            var password:String= binding.edittextPassword.text.toString()
+            clickCreateAccount(email, password)
+        }
+    }
+
+    fun clickCreateAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val intent = Intent(context, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    print("Voy a llorar")
+                }
+            }
     }
 }
